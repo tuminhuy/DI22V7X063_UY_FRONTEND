a7e0d50216a8c3b2085f3552bc9b429dde4d673b
tuminhuy <tuminhuy21@gmail.com>
2023-12-19 21:21:45 +0700

    HT

--- contactbook-frontend/src/main.js
+++ contactbook-frontend/src/main.js
@@ -1,16 +1,8 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-<<<<<<< HEAD
 import router from './router';
 
 createApp(App).use(router).mount('#app')
-=======
-import router from "./router";
 
-createApp(App).mount('#app')
-createApp(App).use(router).mount("#app");
->>>>>>> f0bd00aeecad63bd0f1ab1498673a424e6759436
