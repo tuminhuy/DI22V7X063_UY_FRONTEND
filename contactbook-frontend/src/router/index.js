8aa0166eaef58647749323618627b7328483118c
tuminhuy <tuminhuy21@gmail.com>
2023-12-19 21:35:57 +0700

    Chinh Index

--- contactbook-frontend/src/router/index.js
+++ contactbook-frontend/src/router/index.js
@@ -1,46 +1,28 @@
-<<<<<<< HEAD
 import { createWebHistory, createRouter } from "vue-router";
 import ContactBook from "@/views/ContactBook.vue";
+
 const routes = [
-{
-path: "/",
-name: "contactbook",
-component: ContactBook,
-},
-];
-const router = createRouter({
-history: createWebHistory(import.meta.env.BASE_URL),
-routes,
-});
-export default router;
-=======
-import { createWebHistory, createRouter } from "vue-router";
-import ContactBook from "@/views/ContactBook.vue";
-const routes = [
-{
-path: "/",
-name: "contactbook",
-component: ContactBook,
-},
-{
-path: "/:pathMatch(.*)*",
-name: "notfound",
-component: () => import("@/views/NotFound.vue"),
-},
-];
-const router = createRouter({
-history: createWebHistory(import.meta.env.BASE_URL),
-routes,
-});
-const routes = [
-{
-path: "/contacts/:id",
-name: "contact.edit",
-component: () => import("@/views/ContactEdit.vue"),
-props: true // Truyền các biến trong $route.params vào làm props
-},
+  {
+    path: "/",
+    name: "contactbook",
+    component: ContactBook,
+  },
+  {
+    path: "/contacts/:id", // Đường dẫn với tham số động ":id"
+    name: "contact.edit",
+    component: () => import("@/views/ContactEdit.vue"),
+    props: true, // Truyền các biến trong $route.params vào làm props
+  },
+  {
+    path: "/:pathMatch(.*)*", // Tất cả các đường dẫn không khớp với các tuyến đường trên sẽ dẫn đến NotFound
+    name: "notfound",
+    component: () => import("@/views/NotFound.vue"),
+  },
 ];
 
-export default router;
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+});
 
->>>>>>> f0bd00aeecad63bd0f1ab1498673a424e6759436
+export default router;
