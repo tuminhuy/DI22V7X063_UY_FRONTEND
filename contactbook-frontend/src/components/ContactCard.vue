a4fc720db9cfff64b3e0f8bb0baf5f72c6f39729
tuminhuy <tuminhuy21@gmail.com>
2023-12-19 21:10:32 +0700

    Tao trang cap nhat lien he

--- contactbook-frontend/src/components/ContactCard.vue
+++ contactbook-frontend/src/components/ContactCard.vue
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-<script>
-export default {
-props: {
-contact: { type: Object, required: true },
-},
-};
-</script>
-<template>
-<div>
-<div class="p-1">
-<strong>Tên:</strong>
-{{ contact.name }}
-</div>
-<div class="p-1">
-<strong>E-mail:</strong>
-{{ contact.email }}
-</div>
-<div class="p-1">
-<strong>Địa chỉ:</strong>
-{{ contact.address }}
-</div>
-<div class="p-1">
-<strong>Điện thoại:</strong>
-{{ contact.phone }}
-</div>
-<div class="p-1">
-<strong>Liên hệ yêu thích:&nbsp;</strong>
-<i v-if="contact.favorite" class="fas fa-check"></i>
-<i v-else class="fas fa-times"></i>
-</div>
-</div>
-</template>
-=======
 <script>
 export default {
     props: {
@@ -39,6 +5,7 @@ export default {
     },
 };
 </script>
+
 <template>
     <div>
         <div class="p-1">
@@ -62,6 +29,12 @@ export default {
             <i v-if="contact.favorite" class="fas fa-check"></i>
             <i v-else class="fas fa-times"></i>
         </div>
+
+        <!-- Thêm đường liên kết đến trang hiệu chỉnh -->
+        <router-link :to="{ name: 'editContact', params: { id: contact.id } }">
+            <button class="btn btn-sm btn-warning">
+                <i class="fas fa-edit"></i> Hiệu chỉnh
+            </button>
+        </router-link>
     </div>
 </template>
->>>>>>> f0bd00aeecad63bd0f1ab1498673a424e6759436
--- contactbook-frontend/src/router/index.js
+++ contactbook-frontend/src/router/index.js
@@ -32,6 +32,15 @@ const router = createRouter({
 history: createWebHistory(import.meta.env.BASE_URL),
 routes,
 });
+const routes = [
+{
+path: "/contacts/:id",
+name: "contact.edit",
+component: () => import("@/views/ContactEdit.vue"),
+props: true // Truyền các biến trong $route.params vào làm props
+},
+];
+
 export default router;
 
 >>>>>>> f0bd00aeecad63bd0f1ab1498673a424e6759436
