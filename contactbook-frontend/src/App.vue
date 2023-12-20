b2305fd2d181e36f209eae3cd1b9f897baf1dd7e
tuminhuy <tuminhuy21@gmail.com>
2023-12-20 21:13:38 +0700

    HT

--- contactbook-frontend/src/App.vue
+++ contactbook-frontend/src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+//import TheWelcome from './components/TheWelcome.vue'
 import AppHeader from "@/components/AppHeader.vue";
 </script>
 
--- contactbook-frontend/vite.config.js
+++ contactbook-frontend/vite.config.js
@@ -1,5 +1,3 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -8,25 +6,19 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
- /* resolve: {
+  resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  */
- resolve: {
-        alias: {
-            '@': '/src',
-        },
+      '@': '/src',
     },
+  },
   server: {
     port: 3001,
-  
-  proxy: {
-    "/api": {
-      target: "http://localhost:3000/",
-      changeOrigin: true,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000/",
+        changeOrigin: true,
+      },
     },
-  }
-},
+  },
 });
+
